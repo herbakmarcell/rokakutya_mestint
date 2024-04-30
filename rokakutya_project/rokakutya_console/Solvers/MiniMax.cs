f66405f4435aba62365d1e3fea5eba1f8df87797
herbakmarcell <herbakmarcell@gmail.com>
2024-04-30 17:32:53 +0200

    minimax solver

---
+++ rokakutya_project/rokakutya_console/Solvers/MiniMax.cs
@@ -0,0 +1,45 @@
+﻿using rokakutya_console.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rokakutya_console.Solvers
+{
+    public class MiniMax : Solver
+    {
+        public int Depth { get; set; }
+
+        public MiniMax(OperatorGenerator operatorGenerator, int depth) : base(operatorGenerator)
+        {
+            Depth = depth;
+        }
+
+        public override State NextMove(State state)
+        {
+            Node currentNode = new Node(state);
+            ExtendNode(currentNode);
+
+            currentNode.SortChildrenMiniMax(state.CurrentPlayer);
+
+            return currentNode.Children[0].State;
+
+        }
+
+        private void ExtendNode(Node node)
+        {
+            if (node.State.GetStatus() != Status.PLAYING || node.Depth >= Depth) return;
+            foreach (Operator o in Operators)
+            {
+                if (o.IsApplicable(node.State))
+                {
+                    State newState = o.Apply(node.State);
+                    Node newNode = new Node(newState, node);
+                    node.Children.Add(newNode);
+                    ExtendNode(newNode);
+                }
+            }
+        }
+    }
+}
